5560d280d642a942cdc690868f34968a29b21188
matt.burdumy <matt.burdumy@craftsy.com>
2016-12-05 23:04:35 -0700

    stuck on getting the signature to match....

--- amazon-website/lambda/lambda/hello_lambda/getSimilarProducts.py
+++ amazon-website/lambda/lambda/hello_lambda/getSimilarProducts.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 import boto3
-import json
+import json, requests, base64, urllib, hashlib, hmac, datetime, pytz
 
 print('Loading function')
 
@@ -43,12 +43,43 @@ def lambda_handler(event, context):
     # else:
     #     return respond(ValueError('Unsupported method "{}"'.format(operation)))
 
+def make_request():
+    endpoint = 'webservices.amazon.com'
+    uri = "/onca/xml"
+    url = 'http://webservices.amazon.com/onca/xml'
+    awsSecretKey = 'VSYix7/jZOMY9L26yboAI4I63dCx9HHUBhDW1qUa'
+    timestamp = datetime.datetime.now(tz=pytz.UTC).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-4] + "Z"
+    params = {
+        'Service':'AWSECommerceService',
+        'Operation': 'ItemSearch',
+        'AWSAccessKeyId': 'AKIAJ7OJKPO3XIUZ6V4A',
+        'AssociateTag': 'bestbo03-20',
+        'Keywords': 'Harry Potter',
+        'ResponseGroup': "Images,ItemAttributes,Offers",
+        "Sort" : "price",
+        "Timestamp": timestamp
+    }
+    keys = sorted(params)
+    slug = ""
+    for key in keys:
+        slug = slug + urllib.quote_plus(key) + '=' + urllib.quote_plus(params[key])
+        if key != keys[-1]:
+            slug = slug + '&'
 
+    string_to_sign = "GET\n" + endpoint + "\n" + uri + "\n" + slug
+    sha_hash = hmac.new(key=awsSecretKey, msg=string_to_sign,digestmod=hashlib.sha256)
+    signature = base64.b64encode(sha_hash.digest())
+    params['Signature'] = signature
+
+    r = requests.get(url=url, params=params)
+    print(r.text)
+    pass
 def main():
-    with open('test-event.json', 'r') as fp:
-        data = json.load(fp)
-        response = lambda_handler(event = data, context=None)
-        print(response)
+    # with open('test-event.json', 'r') as fp:
+    #     data = json.load(fp)
+    #     response = lambda_handler(event = data, context=None)
+    #     print(response)
+    make_request()
 
 if __name__ == '__main__':
     main()
