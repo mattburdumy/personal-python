1ebce84d2bd9cbe102f6e6debb0039e7e185016e
matt.burdumy <matt.burdumy@craftsy.com>
2016-12-27 14:40:06 -0700

    got js lambda working. saving work before testing s3 sync command

--- amazon-website/api_gateway_lambda/apiGateway.py
+++ amazon-website/api_gateway_lambda/apiGateway.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 import boto3
-import json, requests, base64, urllib, hashlib, hmac, datetime, pytz
+import json
 
 print('Loading function')
 
@@ -9,9 +9,9 @@ print('Loading function')
 def respond(err, res=None):
     return {
         'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(res),
+        'body': err.message if err else res,
         'headers': {
-            'Content-Type': 'application/json',
+            'Content-Type': 'text/html',
         },
     }
 
@@ -27,7 +27,8 @@ def lambda_handler(event, context):
     DynamoDB API as a JSON body.
     '''
     #print("Received event: " + json.dumps(event, indent=2))
-    return respond(None,event['test'])
+    # return respond(err=None,res=get_index_file())
+    return respond(err=None,res=get_index_file())
     # operations = {
     #     'DELETE': lambda dynamo, x: dynamo.delete_item(**x),
     #     'GET': lambda dynamo, x: dynamo.scan(**x),
@@ -43,45 +44,19 @@ def lambda_handler(event, context):
     # else:
     #     return respond(ValueError('Unsupported method "{}"'.format(operation)))
 
-def make_request():
-    endpoint = 'webservices.amazon.com'
-    uri = "/onca/xml"
-    url = 'http://webservices.amazon.com/onca/xml'
-    awsSecretKey = 'VSYix7/jZOMY9L26yboAI4I63dCx9HHUBhDW1qUa'
-    timestamp = datetime.datetime.now(tz=pytz.UTC).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-4] + "Z"
-    params = {
-        'Service':'AWSECommerceService',
-        'Operation': 'ItemSearch',
-        'AWSAccessKeyId': 'AKIAJ7OJKPO3XIUZ6V4A',
-        'AssociateTag': 'bestbo03-20',
-        'Keywords': 'Harry+Potter',
-        'SearchIndex':'Books',
-        'ResponseGroup': "Images,ItemAttributes,Offers",
-        "Sort" : "price",
-        "Timestamp": timestamp
-    }
-    keys = sorted(params)
-    slug = ""
-    for key in keys:
-        slug = slug + urllib.quote_plus(key) + '=' + urllib.quote_plus(params[key])
-        if key != keys[-1]:
-            slug = slug + '&'
+def get_index_file():
+    session = boto3.Session()
+    client = session.client('s3')
+    obj =client.get_object(Bucket='recowebsite', Key='dummyData.txt')
+    return (obj['Body'].read())
 
-    string_to_sign = "GET\n" + endpoint + "\n" + uri + "\n" + slug
-    sha_hash = hmac.new(key=awsSecretKey, msg=string_to_sign,digestmod=hashlib.sha256)
-    signature = base64.b64encode(sha_hash.digest())
-    params['Signature'] = signature
 
-    r = requests.get(url=url, params=params)
-    print(r.text)
-    session = boto3.Session(profile_name='personal')
-    pass
 def main():
     # with open('test-event.json', 'r') as fp:
     #     data = json.load(fp)
-    #     response = lambda_handler(event = data, context=None)
+    #     response = lambda_handler(event=data, context=None)
     #     print(response)
-    make_request()
+    print(get_index_file())
 
 if __name__ == '__main__':
     main()
